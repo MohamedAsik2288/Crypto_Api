e88e5e5335dd8315a3ff1f941f91343caaf5b50d
Mohamed Asik <er.mohamedasik@gmail.com>
2023-06-05 13:29:04 +0530

    Crypto_Project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
 
+import { useEffect, useState } from 'react';
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from 'axios';
+import ImageCard from './components/ImageCard';
 function App() {
+
+  const [currencyList,setCurrencylist]=useState([]);
+
+const [search,setSearch]=useState('');
+
+const handleSearch= (e)=> setSearch(e.target.value);
+
+
+  useEffect(()=> {
+     axios.get('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd').then((response)=>setCurrencylist(response.data))
+  },[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <div className='header p-2'>
+      <h2> CryptoCurrency Tracker</h2>
+<div className='row justify-content-center'>
+  <div className="col-xs-12 col-sm-12 col-md-6 col-lg-5 col-xl-4 mt-3 mb-3">
+ <input class="form-control me-2" type="search" placeholder="Search..." aria-label="Search"
+  onChange={handleSearch}
+ />
+  </div>
+</div>
+
+     
+     </div>
+     <div className='p-5 content d-flex flex-wrap justify-content-center mt-1' >
+      {
+        currencyList.filter((currency)=>currency.name.toLowerCase().includes(search.toLowerCase()))
+          
+         .map((currency)=>(<ImageCard{ ...currency}/>))
+      }
+     </div>
     </div>
   );
 }
---
+++ src/components/ImageCard.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+ export default function ImageCard(props) {
+  return (
+    <div className='p-2'>
+        <div class="card pt-2" style={{width: 300}}>
+
+        <div className='justify-content-center'>
+        <img src={props.image}  alt={props.name}
+            width='80' height='80'
+                />
+
+        </div>
+  
+  <div class="card-body">
+    <h5 class="card-title">{props.name}</h5>
+    <p>
+Price:{`$${props.current_price}`}
+ </p>
+
+ <p>
+Rank:{`$${props.market_cap_rank}`}
+ </p>
+
+ <p>
+Market  Cap:{`$${props.market_cap}`}
+ </p>
+
+
+  </div>
+</div>
+    </div>
+  )
+}
+
